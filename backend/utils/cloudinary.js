efcdd2b82c81876912993a435d36d8ab9ed413eb
Rohit Dubey <137141150+Rohit7075@users.noreply.github.com>
2024-07-30 21:19:32 +0530

    Update cloudinary.js

--- backend/utils/cloudinary.js
+++ backend/utils/cloudinary.js
@@ -3,8 +3,8 @@ import dotenv from "dotenv";
 dotenv.config();
 
 cloudinary.config({
-    cloud_name:dwe5afy4r,
+    cloud_name:"dwe5afy4r",
     api_key:762543142417586,
-    api_secret:ZKX9drodfcXLs-KN7vm4Wxj0teM
+    api_secret:"ZKX9drodfcXLs-KN7vm4Wxj0teM"
 });
 export default cloudinary;
