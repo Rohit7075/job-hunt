2b112b4f987c6a50db02bd2756730031d5c675ac
Rohit Dubey <137141150+Rohit7075@users.noreply.github.com>
2024-07-30 21:20:24 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,7 +7,7 @@ import { Provider } from 'react-redux'
 import store from './redux/store.js'
 import { persistStore } from 'redux-persist'
 import { PersistGate } from 'redux-persist/integration/react'
-
+export const index="http://localhost:8080/"
 const persistor = persistStore(store);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
